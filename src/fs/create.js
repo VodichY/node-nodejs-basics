3c3e3c155d7ebe3dea8a8d3c96825d8f5a0ad153
Yuriy Vodich <yuriy.vodich@onetwotrip.com>
2025-04-29 03:20:37 +0300

    FILE-SYSTEM: Implemented function to create new file fresh.txt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const dataDir = path.resolve(__dirname, '..', 'fs', 'files');
+  const fileName = 'fresh.txt';
+  const filePath = path.join(dataDir, fileName);
+
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.writeFile(filePath, content);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
